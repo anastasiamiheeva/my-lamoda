8483d19101aa7c836ac8666767c5a594e4387b73
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2021-07-07 17:40:47 +0300

    day2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,8 @@
 const headerCityButton = document.querySelector('.header__city-button');
 
-headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
+let hash = location.hash.substring(1);
 
+headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
 headerCityButton.addEventListener('click', () => {
   const city = prompt('Укажите Ваш город');
   headerCityButton.textContent = city;
@@ -31,7 +32,6 @@ const enableScroll = () => {
 }
 
 //modal window
-
 const subheaderCart = document.querySelector('.subheader__cart');
 const cartOverlay = document.querySelector('.cart-overlay');
 
@@ -45,6 +45,40 @@ const cartModalClose = () => {
   enableScroll();
 }
 
+//Запрос базы данных
+
+const getData = async () => {
+  const data = await fetch('db.json');
+
+  if(data.ok) {
+    return data.json()
+  } else {
+    throw new Error(`Данные не были получены, ошибка: ${data.status} ${data.statusText}`)
+  }
+}
+/*getData()
+.then(data => {
+  console.log(data)
+})
+.catch(err => {
+  console.log(err)
+})*/
+
+const getGoods = (callback, value) => {
+  getData()
+  .then(data => {
+    if (value) {
+        callback(data.filter(item => item.category === value))
+    } else {
+        callback(data);
+    }
+  })
+  .catch(err => {
+    console.error(err)
+  })
+}
+
+
 subheaderCart.addEventListener('click', cartModalOpen);
 cartOverlay.addEventListener('click', event => {
   const target = event.target;
@@ -52,3 +86,53 @@ cartOverlay.addEventListener('click', event => {
     cartModalClose();
   }
 })
+
+try {
+  const goodList = document.querySelector('.goods__list');
+  if(!goodList) {
+    throw 'This is not goods page'
+  }
+
+  const createCard = ({ id, preview, cost, brand, name, sizes }) => {
+    const li = document.createElement('li');
+
+    li.classList.add('goods__item');
+    li.innerHTML = `
+    <article class="good">
+      <a class="good__link-img" href="card-good.html#${id}">
+          <img class="good__img" src="goods-image/${preview}" alt="">
+      </a>
+      <div class="good__description">
+          <p class="good__price">${cost} &#8381;</p>
+          <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+          ${sizes 
+              ? `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>` 
+              : ''}
+          <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+      </div>
+    </article>
+    `;
+
+    return li;
+  };
+
+  const renderGoodsList = data => {
+    goodList.textContent = '';
+
+    data.forEach (item => {
+     const card = createCard(item);
+     goodList.append(card)
+    })
+  }
+
+  window.addEventListener('hashchange', () => {
+    hash = location.hash.substring(1)
+    getGoods(renderGoodsList, hash);
+  })
+  
+  getGoods(renderGoodsList, hash);
+
+}catch (err) {
+  console.warn(err);
+}
+
